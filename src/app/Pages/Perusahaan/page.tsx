89279225718ca61e28e49c3378d2e97405c1f695
Ian M. <amazeianproxy@gmail.com>
2025-03-10 22:20:07 +0700

    Add files via upload

--- src/app/Pages/Perusahaan/page.tsx
+++ src/app/Pages/Perusahaan/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 
 import Header from "@/app/Component/header";
+import CompanyCard from "@/app/Component/CompanyBlock";
 
 const Perusahaan = () => {
     return(
         <div>
             <Header/>
             <div className="text-black">
-                list perusahaan
+                List Perusahaan
+            </div>
+            <div className="flex justify-center items-center">
+                <CompanyCard />
+            </div>
+            <br></br>
+            <div className="flex justify-center items-center">
+                <CompanyCard />
+            </div>
+            <br></br>
+            <div className="flex justify-center items-center">
+                <CompanyCard />
             </div>
         </div>
+        
     );
 };
 
