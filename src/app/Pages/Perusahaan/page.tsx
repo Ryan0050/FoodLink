05be726500b74f8dfa02d0746cca861e149f9e57
Ryan0050 <lawyran0511@gmail.com>
2025-03-15 18:29:04 +0700

    .

--- src/app/Pages/Perusahaan/page.tsx
+++ src/app/Pages/Perusahaan/page.tsx
@@ -7,9 +7,6 @@ const Perusahaan = () => {
     return(
         <div>
             <Header/>
-            <div className="text-black">
-                List Perusahaan
-            </div>
             <div className="flex justify-center items-center">
                 <CompanyCard />
             </div>
